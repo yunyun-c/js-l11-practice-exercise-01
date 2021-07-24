aa7bafbdbd48b2e7a3dfbf74d4cbc6a62bb4df0d
yunyun-c <82518667+yunyun-c@users.noreply.github.com>
2021-07-25 00:39:51 +0800

    Exercises- intro to APIs-01-02

--- js/index.js
+++ js/index.js
@@ -1,1 +1,36 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
+
+const getData = async function (numUsers) {
+  const usersRequest = await fetch(
+    `https://randomuser.me/api?results=${numUsers}`
+  );
+  const data = await usersRequest.json();
+
+  const userResults = data.results;
+  displayUsers(userResults);
+};
+
+getData(1);
+
+const displayUsers = function (userResults) {
+  randomFolks.innerHTML = "";
+
+  for (const user of userResults) {
+    const country = user.location.country;
+    const name = user.name.first;
+    const imageUrl = user.picture.medium;
+    const userDiv = document.createElement("div");
+    userDiv.innerHTML = `
+            <h3>${name}</h3>
+            <p>${country}</p>
+            <img src=${imageUrl} alt="User avatar" />
+        `;
+    randomFolks.append(userDiv);
+  }
+};
+
+selectUserNumber.addEventListener("change", function (e) {
+  const numUsers = e.target.value;
+  getData(numUsers);
+});
